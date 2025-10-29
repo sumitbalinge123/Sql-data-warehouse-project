4335f894daf62ea07d755857ac7dee453f1517fd
Sumit Balinge <157221293+sumitbalinge123@users.noreply.github.com>
2025-10-29 20:54:21 +0530

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -18,6 +18,31 @@ Usage:
 -- Create Dimension: gold.dim_customers
 -- =============================================================================
 
+select *
+from silver.crm_cust_info
+
+--check duplicate
+select cst_id, count(*) from
+	(select
+		ci.cst_id,
+		ci.cst_key,
+		ci.cst_firstname,
+		ci.cst_material_status,
+		ci.cst_gndr,
+		ci.cst_create_date,
+		ca.bdate,
+		ca.gen,
+		la.cntry
+	from silver.crm_cust_info ci
+	left join silver.erp_cust_az12 ca
+	on ci.cst_key = ca.cid
+	left join silver.erp_loc_a101 la
+	on ci.cst_key = la.cid
+)t GROUP BY cst_id
+HAVING COUNT(*) > 1
+
+
+--solution for duplicates
 create view gold.dim_customers as
 SELECT 
     ROW_NUMBER() over (order by cst_id) AS customer_key,
@@ -52,11 +77,44 @@ FROM (
 ) t
 WHERE rn = 1;
 
+select
+distinct gender 
+from gold.dim_customers
+
+select distinct
+ci.cst_gndr,
+ca.gen,
+case when ci.cst_gndr != 'n/a' then ci.cst_gndr   --CRM IS THE MASTER FOR GENDER INFO
+ELSE COALESCE(ca.gen, 'n/a')
+END AS new_gen
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+lEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid
+
 
 -- =============================================================================
 -- Create Dimension: gold.dim_products
 -- =============================================================================
 
+select prd_id, count(*) from
+	(select
+	pn.prd_id,
+	pn.prd_key,
+	pn.cat_id,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt
+	from silver.crm_prd_info pn
+	left join silver.erp_px_cat_g1v2 pc
+	on pn.cat_id = pc.id
+	where prd_end_dt IS NULL    --filter out all historical data
+)t GROUP BY prd_id
+HAVING COUNT(*) > 1
+
+--SOLUTION FOR DUPLICATE	
 --create view
 
 CREATE VIEW gold.dim_products as
